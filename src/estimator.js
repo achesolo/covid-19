fedc4664b449dc94d954c8bf327332fdeacd4749
unknown <achesolo@gmail.com>
2020-04-12 17:36:37 +0000

    new changes

--- src/estimator.js
+++ src/estimator.js
@@ -26,7 +26,8 @@ const Impact = (data) => {
   const currentlyInfectedSevere = Math.trunc(data.reportedCases * 50);
 
   const infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** returnPeriod(data)));
-  const infectionsByRequestedTimeSevere = Math.trunc(currentlyInfectedSevere * (2 ** returnPeriod(data)));
+  const infectionsByRequestedTimeSevere = Math.trunc(currentlyInfectedSevere
+     * (2 ** returnPeriod(data)));
 
   const severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime);
   const severeCasesByRequestedTimeSevere = Math.trunc(0.15 * infectionsByRequestedTimeSevere);

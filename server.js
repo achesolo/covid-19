55c9b154b8a88d6637d173fc53e8b848aa3306b4
unknown <achesolo@gmail.com>
2020-04-14 09:40:09 +0000

    new changes

--- server.js
+++ server.js
@@ -31,8 +31,7 @@
 //   app.use('/api/v1/on-covid-19', covidRouters);
 //   module.exports = app;
 
-const express = require('express')
-const app = express()
+
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))

e091b556cc453cc6a3ecd8c4dfa685367e55b94e
unknown <achesolo@gmail.com>
2020-04-14 09:31:45 +0000

    new changes

--- router.js
+++ router.js
@@ -1,39 +1,58 @@
-const express = require('express');
-const convert = require('xml-js');
-const covidEstimator = require('./src/estimator');
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const app = express();
+// const express = require('express');
+// const convert = require('xml-js');
+// const covidEstimator = require('./src/estimator');
+// const bodyParser = require('body-parser');
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// const app = express();
 
-//const router = app.Router();
+// //const router = app.Router();
+
+// // middleware that is specific to this router
+// app.use('logs', (req, res, next) => {
+//   const startTime = new Date().getTime();
+//   const reqTime = new Date().getTime() - startTime;
+
+//   req.headers('Content-Type', 'text/plain');
+//   res.send((`${Date.now()}\t\t${req.path}\t\t ${res.sendStatus(200)}\t\t${reqTime}ms\n`));
+//   next();
+// });
+
+// // define the home page route
+// app.get('/api/v1/on-covid-19/', (req, res) => {
+//  // res.end(covidEstimator(req.body));
+//  res.send("heelo body")
+// });
+// // define the about route
+// app.post('/api/v1/on-covid-19/', urlencodedParser, (req, res) => {
+//   res.end(covidEstimator(req.body));
+// });
+// app.post('/api/v1/on-covid-19/api/v1/on-covid-19/json', urlencodedParser, (req, res) => {
+//   res.json(covidEstimator(req.body));
+// });
+// app.post('/api/v1/on-covid-19/xml', urlencodedParser, (req, res) => {
+//   req.headers('Content-Type', 'application/xml; charset=UTF-8');
+//   const options = { compact: true, ignoreComment: true, spaces: 4 };
+//   const xmlResult = convert.json2xml((covidEstimator(req.body), options));
+//   res.send(xmlResult);
+// });
+
+// module.exports = app;
+
+var express = require('express')
+var router = express.Router()
 
 // middleware that is specific to this router
-app.use('logs', (req, res, next) => {
-  const startTime = new Date().getTime();
-  const reqTime = new Date().getTime() - startTime;
-
-  req.headers('Content-Type', 'text/plain');
-  res.send((`${Date.now()}\t\t${req.path}\t\t ${res.sendStatus(200)}\t\t${reqTime}ms\n`));
-  next();
-});
-
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
 // define the home page route
-app.get('/api/v1/on-covid-19/', (req, res) => {
- // res.end(covidEstimator(req.body));
- res.send("heelo body")
-});
+router.get('/', function (req, res) {
+  res.send('Birds home page')
+})
 // define the about route
-app.post('/api/v1/on-covid-19/', urlencodedParser, (req, res) => {
-  res.end(covidEstimator(req.body));
-});
-app.post('/api/v1/on-covid-19/api/v1/on-covid-19/json', urlencodedParser, (req, res) => {
-  res.json(covidEstimator(req.body));
-});
-app.post('/api/v1/on-covid-19/xml', urlencodedParser, (req, res) => {
-  req.headers('Content-Type', 'application/xml; charset=UTF-8');
-  const options = { compact: true, ignoreComment: true, spaces: 4 };
-  const xmlResult = convert.json2xml((covidEstimator(req.body), options));
-  res.send(xmlResult);
-});
+router.get('/about', function (req, res) {
+  res.send('About birds')
+})
 
-module.exports = app;
+module.exports = router
--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-// const express = require('express');
+// // const express = require('express');
 
-// const covid19Estimator = require('./estimator.js');
+// // const covid19Estimator = require('./estimator.js');
 
-// const app = express();
-
-
-// app.post('/api-v1-on-covid-19', (req, res) => {
-//   const data = req.body;
-//   res.json(covid19Estimator(data));
-// });
-
-const express = require('express');
-const  app = express();
-const cors = require('cors');
-const covidRouters = require('./router');
+// // const app = express();
 
 
-// app.get('/index.html', (req, res) => {
-//     res.sendFile(`${__dirname}/` + 'index.html');
-//   });
+// // app.post('/api-v1-on-covid-19', (req, res) => {
+// //   const data = req.body;
+// //   res.json(covid19Estimator(data));
+// // });
+
+ const express = require('express');
+ const  app = express();
+// const cors = require('cors');
+// const covidRouters = require('./router');
+
+
+// // app.get('/index.html', (req, res) => {
+// //     res.sendFile(`${__dirname}/` + 'index.html');
+// //   });
   
 
-//
-    const server = app.listen(8081, () => {
-    const host = server.address().address;
-    const { port } = server.address();
-     console.log('app listening at {0} on {1} ',host, port);
-  });
-  app.use(cors());
-  app.use('/api/v1/on-covid-19', covidRouters);
-  module.exports = app;
+// //
+//     const server = app.listen(8081, () => {
+//     const host = server.address().address;
+//     const { port } = server.address();
+//      console.log('app listening at {0} on {1} ',host, port);
+//   });
+//   app.use(cors());
+//   app.use('/api/v1/on-covid-19', covidRouters);
+//   module.exports = app;
+
+var birds = require('./router')
+
+// ...
+
+app.use('/birds', birds)

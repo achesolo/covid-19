23107438d5be72a12fd318390438374daa001234
unknown <achesolo@gmail.com>
2020-04-14 15:11:41 +0000

    new changes

--- router.js
+++ router.js
@@ -18,7 +18,7 @@ router.use('/logs', (req, res, next) => {
 // define the home page route
 router.post('/', urlencodedParser, (req, res) => {
  // fs.readFile(`${__dirname}/` + './src/inputData.json', 'utf8', (err, data) => {
-    const estimator = covidEstimator(req.body.data);
+    const estimator = covidEstimator(req.body);
     res.end(estimator);
  // });
 });
